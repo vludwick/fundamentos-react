7156d1fcf3f02c0783979516a097ceca4f8c3306
Victor Ludwick <victor_ludwick@hotmail.com.br>
2022-12-06 15:10:37 -0300

    Correção desafio número aleatório

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Aleatorio from "./components/basicos/Aleatorio";
 export default () => (
   <div>
     <h1>Fundamentos React</h1>
-    <Aleatorio min={1} max={10}/>
+    <Aleatorio min={1} max={60}/>
     <Fragmento />
     <ComParametro titulo="Segundo Componente" subtitulo="Muito legal!" />
     <Primeiro />
--- src/components/basicos/Aleatorio.jsx
+++ src/components/basicos/Aleatorio.jsx
@@ -2,10 +2,21 @@
 import React from "react";
 
 export default (props) => {
+  const { min, max } = props;
+  const aleatorio = parseInt(Math.random() * (max - min)) + min;
   return (
-    <>
-      <h2>Aleatório</h2>
-      <h3>{Math.floor(Math.random() * (props.max + 1)) + props.min}</h3>
-    </>
+    <div>
+      <h2>Valor Aleatório</h2>
+      <p>
+        <strong>Valor Mínimo:</strong> {min}
+      </p>
+      <p>
+        <strong>Valor Máximo:</strong> {max}
+      </p>
+      <p>
+        <strong>Valor Escolhido:</strong> {aleatorio}
+      </p>
+      <strong></strong>
+    </div>
   );
 };
