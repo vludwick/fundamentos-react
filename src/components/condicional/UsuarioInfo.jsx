8bb3df1d517e051c5a2eb710a02edebb3dff9773
Victor Ludwick <victor_ludwick@hotmail.com.br>
2022-12-15 08:49:41 -0300

    Criando componente Else

--- src/components/condicional/UsuarioInfo.jsx
+++ src/components/condicional/UsuarioInfo.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-anonymous-default-export */
 import React from "react";
 
-import If from "./If";
+import If, { Else } from "./If";
 
 export default (props) => {
   const usuario = props.usuario || {};
@@ -10,9 +10,9 @@ export default (props) => {
     <div>
       <If test={usuario && usuario.nome}>
         Seja bem vindo <strong>{usuario.nome}</strong>!
-      </If>
-      <If test={!usuario || !usuario.nome}>
-        Seja bem vindo <strong>Amigão</strong>!
+        <Else>
+          Seja bem vindo <strong>Amigão</strong>!
+        </Else>
       </If>
     </div>
   );
