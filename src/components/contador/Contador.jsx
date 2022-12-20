ab0bee530d3eeeab54926ef6af334114c6ca355e
Victor Ludwick <victor_ludwick@hotmail.com.br>
2022-12-20 09:49:09 -0300

    Separando o contador em mais componentes

--- src/components/contador/Contador.jsx
+++ src/components/contador/Contador.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
+import Display from "./Display";
+import Botoes from "./Botoes";
+import PassoForm from "./PassoForm";
 
-import "./Contador.css"
+import "./Contador.css";
 
 class Contador extends Component {
   state = {
@@ -16,20 +19,17 @@ class Contador extends Component {
     this.setState({ numero: this.state.numero - this.state.passo });
   };
 
-  setPasso = (e) => {
-    this.setState({ passo: parseInt(e.target.value) });
-  }
+  setPasso = (novoPasso) => {
+    this.setState({ passo: novoPasso });
+  };
 
   render() {
     return (
       <div className="contador">
         <h2>Contador</h2>
-        <h3>{this.state.numero}</h3>
-        <div>
-            <label htmlFor="passoInput">Passo: </label>
-            <input type="number" id="passoInput" value={this.state.passo} onChange={this.setPasso}/></div>
-        <button onClick={this.inc}>+</button>
-        <button onClick={this.dec}>-</button>
+        <Display numero={this.state.numero} />
+        <PassoForm passo={this.state.passo} setPasso={this.setPasso} />
+        <Botoes setInc={this.inc} setDec={this.dec} />
       </div>
     );
   }
---
+++ src/components/contador/PassoForm.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React from "react";
+
+export default (props) => {
+  return (
+    <div>
+      <label htmlFor="passoInput">Passo: </label>
+      <input
+        type="number"
+        id="passoInput"
+        value={props.passo}
+        onChange={(e) => props.setPasso(+e.target.value)}
+      />
+    </div>
+  );
+};
